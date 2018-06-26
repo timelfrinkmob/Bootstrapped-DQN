c777bbeed263e1c9d541bd2181460e280154a455
Tim Elfrink <telfrink@mobiquityinc.com>
2018-06-26 22:26:29 +0200

    Create results_chain.sh

---
+++ results_chain.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+for seed in 1 2 3
+do
+    for i in 10 20 30 40 50 60 70 80 90 100;
+    do
+	    for exp in "--bootstrap --no-noisy --no-greedy" "--no-bootstrap --noisy --no-greedy" "--no-bootstrap --no-noisy --no-greedy"  "--no-bootstrap --no-noisy --greedy" ;
+	    do
+            python train_gym.py --n=$i $exp --seed=$seed
+        done
+    done
+done
+
+aws s3 cp ./models/ s3://thesis-tim-files/baseline/chain/ --recursive
+sudo shutdown -P now
