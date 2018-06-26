3cfa19511cfbc00b30d121574f263c5e40497cfb
Tim Elfrink <telfrink@mobiquityinc.com>
2018-06-26 22:09:48 +0200

    Update train_chain.py

--- train_chain.py
+++ train_chain.py
@@ -1,6 +1,19 @@
 import argparse
 import gym
 import gym_chain
+
+import logging
+from gym.envs.registration import register
+
+logger = logging.getLogger(__name__)
+
+register(
+    id='Chainbla-v0',
+    entry_point='gym_chain.envs:ChainEnv',
+)
+
+
+
 import numpy as np
 import os
 import tensorflow as tf
