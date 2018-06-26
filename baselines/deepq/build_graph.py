b2fdabdaa75a80c24920740b6c06d0367a62bce4
Tim Elfrink <telfrink@mobiquityinc.com>
2018-06-26 11:16:38 +0200

    Update build_graph.py

--- baselines/deepq/build_graph.py
+++ baselines/deepq/build_graph.py
@@ -203,7 +203,111 @@ def build_train(make_obs_ph, q_func, num_actions, optimizer, bootstrap=False, gr
     debug: {str: function}
         a bunch of functions to print debug data like q_values.
     """
-    act_f = build_act(make_obs_ph, q_func, bootstrap=bootstrap, num_actions=num_actions, scope=scope, reuse=reuse)
+    if bootstrap:
+        act_f = build_act(make_obs_ph, q_func, bootstrap=bootstrap, num_actions=num_actions, scope=scope, reuse=reuse)
+
+        with tf.variable_scope(scope, reuse=reuse):
+            # set up placeholders
+            obs_t_input = U.ensure_tf_input(make_obs_ph("obs_t"))
+            act_t_ph = tf.placeholder(tf.int32, [None], name="action")
+            rew_t_ph = tf.placeholder(tf.float32, [None], name="reward")
+            obs_tp1_input = U.ensure_tf_input(make_obs_ph("obs_tp1"))
+            done_mask_ph = tf.placeholder(tf.float32, [None], name="done")
+            importance_weights_ph = tf.placeholder(tf.float32, [None], name="weight")
+            update_lr_ph = tf.placeholder(tf.float32, (), name="learning_rate")
+
+            lr = tf.get_variable("lr", (), initializer=tf.constant_initializer(0))
+
+            with tf.device("/gpu:0"):
+                # q network evaluation
+                q_t = q_func(obs_t_input.get(), num_actions, scope="q_func", reuse=True)  # reuse parameters from act
+                q_func_vars = U.scope_vars(U.absolute_scope_name("q_func"))
+
+                # target q network evalution
+                q_tp1 = q_func(obs_tp1_input.get(), num_actions, scope="target_q_func")
+                target_q_func_vars = U.scope_vars(U.absolute_scope_name("target_q_func"))
+
+                # q scores for actions which we know were selected in the given state.
+                q_t_selected = []
+                for i in range(10):
+                    q_t_selected.append(tf.reduce_sum(q_t[i] * tf.one_hot(act_t_ph, num_actions), 1))
+
+                # compute estimate of best possible value starting from state at t + 1
+                q_tp1_best = []
+                q_tp1_best_using_online_net =[]
+                if double_q:
+                    q_tp1_using_online_net = q_func(obs_tp1_input.get(), num_actions, scope="q_func", reuse=True)
+                    for i in range(10):
+                        q_tp1_best_using_online_net.append(tf.arg_max(q_tp1_using_online_net[i], 1))
+                        q_tp1_best.append(tf.reduce_sum(q_tp1[i] * tf.one_hot(q_tp1_best_using_online_net[i], num_actions), 1))
+                else:
+                    for i in range(10):
+                        q_tp1_best.append(tf.reduce_max(q_tp1, 1))
+
+            q_tp1_best_masked =[]
+            q_t_selected_target =[]
+            td_error = []
+            errors = []
+            weighted_error = []
+            optimize_expr = []
+            optimizer = tf.train.AdamOptimizer(learning_rate=lr, beta1=0.9, beta2=0.99, epsilon=1e-4)
+            q_func_heads = U.scope_vars(U.absolute_scope_name("q_func/heads"))
+            q_func_convnets = U.scope_vars(U.absolute_scope_name("q_func/convnet"))
+            for i in range(10):
+                q_tp1_best_masked.append((1.0 - done_mask_ph) * q_tp1_best[i])
+
+                # compute RHS of bellman equation
+                q_t_selected_target.append(rew_t_ph + gamma * q_tp1_best_masked[i])
+
+                # compute the error (potentially clipped)
+                td_error.append(q_t_selected[i] - tf.stop_gradient(q_t_selected_target[i]))
+                with tf.device("/gpu:0"):
+                    errors.append(U.huber_loss(td_error[i]))
+                weighted_error.append(tf.reduce_mean(importance_weights_ph * errors[i]))
+                # compute optimization op (potentially with gradient clipping)
+                if grad_norm_clipping is not None:
+                    optimize_expr.append(U.minimize_and_clip(optimizer,
+                                                        weighted_error[i],
+                                                        var_list=q_func_heads,
+                                                        clip_val=grad_norm_clipping))
+                    optimize_expr.append(U.minimize_and_clip(optimizer,
+                                                        0.1*weighted_error[i],
+                                                        var_list=q_func_convnets,
+                                                        clip_val=grad_norm_clipping))
+                else:
+                    optimize_expr.append(optimizer.minimize(weighted_error[i], var_list=q_func_vars))
+
+            update_lr_expr = lr.assign(tf.cond(update_lr_ph >= 0, lambda: update_lr_ph, lambda: lr))
+            optimize_expr.append(update_lr_expr)
+
+            # update_target_fn will be called periodically to copy Q network to target Q network
+            update_target_expr = []
+            for var, var_target in zip(sorted(q_func_vars, key=lambda v: v.name),
+                                       sorted(target_q_func_vars, key=lambda v: v.name)):
+                update_target_expr.append(var_target.assign(var))
+            update_target_expr = tf.group(*update_target_expr)
+
+            # Create callable functions
+            train = U.function(
+                inputs=[
+                    obs_t_input,
+                    act_t_ph,
+                    rew_t_ph,
+                    obs_tp1_input,
+                    done_mask_ph,
+                    importance_weights_ph,
+                    update_lr_ph
+                ],
+                outputs=td_error[0],
+                updates=optimize_expr,
+            )
+            update_target = U.function([], [], updates=[update_target_expr])
+
+            q_values = U.function([obs_t_input], q_t)
+
+            return act_f, train, update_target, {'q_values': q_values}
+        else:
+                act_f = build_act(make_obs_ph, q_func, num_actions, scope=scope, noisy=noisy, reuse=reuse)
 
     with tf.variable_scope(scope, reuse=reuse):
         # set up placeholders
@@ -213,71 +317,42 @@ def build_train(make_obs_ph, q_func, num_actions, optimizer, bootstrap=False, gr
         obs_tp1_input = U.ensure_tf_input(make_obs_ph("obs_tp1"))
         done_mask_ph = tf.placeholder(tf.float32, [None], name="done")
         importance_weights_ph = tf.placeholder(tf.float32, [None], name="weight")
-        update_lr_ph = tf.placeholder(tf.float32, (), name="learning_rate")
 
-        lr = tf.get_variable("lr", (), initializer=tf.constant_initializer(0))
+        # q network evaluation
+        q_t = q_func(obs_t_input.get(), num_actions, scope="q_func", noisy=noisy, reuse=True)  # reuse parameters from act
+        q_func_vars = U.scope_vars(U.absolute_scope_name("q_func"))
 
-        with tf.device("/gpu:0"):
-            # q network evaluation
-            q_t = q_func(obs_t_input.get(), num_actions, scope="q_func", reuse=True)  # reuse parameters from act
-            q_func_vars = U.scope_vars(U.absolute_scope_name("q_func"))
+        # target q network evalution
+        q_tp1 = q_func(obs_tp1_input.get(), num_actions, scope="target_q_func", noisy=noisy)
+        target_q_func_vars = U.scope_vars(U.absolute_scope_name("target_q_func"))
 
-            # target q network evalution
-            q_tp1 = q_func(obs_tp1_input.get(), num_actions, scope="target_q_func")
-            target_q_func_vars = U.scope_vars(U.absolute_scope_name("target_q_func"))
+        # q scores for actions which we know were selected in the given state.
+        q_t_selected = tf.reduce_sum(q_t * tf.one_hot(act_t_ph, num_actions), 1)
 
-            # q scores for actions which we know were selected in the given state.
-            q_t_selected = []
-            for i in range(10):
-                q_t_selected.append(tf.reduce_sum(q_t[i] * tf.one_hot(act_t_ph, num_actions), 1))
+        # compute estimate of best possible value starting from state at t + 1
+        if double_q:
+            q_tp1_using_online_net = q_func(obs_tp1_input.get(), num_actions, scope="q_func", noisy=noisy, reuse=True)
+            q_tp1_best_using_online_net = tf.arg_max(q_tp1_using_online_net, 1)
+            q_tp1_best = tf.reduce_sum(q_tp1 * tf.one_hot(q_tp1_best_using_online_net, num_actions), 1)
+        else:
+            q_tp1_best = tf.reduce_max(q_tp1, 1)
+        q_tp1_best_masked = (1.0 - done_mask_ph) * q_tp1_best
 
-            # compute estimate of best possible value starting from state at t + 1
-            q_tp1_best = []
-            q_tp1_best_using_online_net =[]
-            if double_q:
-                q_tp1_using_online_net = q_func(obs_tp1_input.get(), num_actions, scope="q_func", reuse=True)
-                for i in range(10):
-                    q_tp1_best_using_online_net.append(tf.arg_max(q_tp1_using_online_net[i], 1))
-                    q_tp1_best.append(tf.reduce_sum(q_tp1[i] * tf.one_hot(q_tp1_best_using_online_net[i], num_actions), 1))
-            else:
-                for i in range(10):
-                    q_tp1_best.append(tf.reduce_max(q_tp1, 1))
+        # compute RHS of bellman equation
+        q_t_selected_target = rew_t_ph + gamma * q_tp1_best_masked
 
-        q_tp1_best_masked =[]
-        q_t_selected_target =[]
-        td_error = []
-        errors = []
-        weighted_error = []
-        optimize_expr = []
-        optimizer = tf.train.AdamOptimizer(learning_rate=lr, beta1=0.9, beta2=0.99, epsilon=1e-4)
-        q_func_heads = U.scope_vars(U.absolute_scope_name("q_func/heads"))
-        q_func_convnets = U.scope_vars(U.absolute_scope_name("q_func/convnet"))
-        for i in range(10):
-            q_tp1_best_masked.append((1.0 - done_mask_ph) * q_tp1_best[i])
-
-            # compute RHS of bellman equation
-            q_t_selected_target.append(rew_t_ph + gamma * q_tp1_best_masked[i])
-
-            # compute the error (potentially clipped)
-            td_error.append(q_t_selected[i] - tf.stop_gradient(q_t_selected_target[i]))
-            with tf.device("/gpu:0"):
-                errors.append(U.huber_loss(td_error[i]))
-            weighted_error.append(tf.reduce_mean(importance_weights_ph * errors[i]))
-            # compute optimization op (potentially with gradient clipping)
-            if grad_norm_clipping is not None:
-                optimize_expr.append(U.minimize_and_clip(optimizer,
-                                                    weighted_error[i],
-                                                    var_list=q_func_heads,
-                                                    clip_val=grad_norm_clipping))
-                optimize_expr.append(U.minimize_and_clip(optimizer,
-                                                    0.1*weighted_error[i],
-                                                    var_list=q_func_convnets,
-                                                    clip_val=grad_norm_clipping))
-            else:
-                optimize_expr.append(optimizer.minimize(weighted_error[i], var_list=q_func_vars))
-
-        update_lr_expr = lr.assign(tf.cond(update_lr_ph >= 0, lambda: update_lr_ph, lambda: lr))
-        optimize_expr.append(update_lr_expr)
+        # compute the error (potentially clipped)
+        td_error = q_t_selected - tf.stop_gradient(q_t_selected_target)
+        errors = U.huber_loss(td_error)
+        weighted_error = tf.reduce_mean(importance_weights_ph * errors)
+        # compute optimization op (potentially with gradient clipping)
+        if grad_norm_clipping is not None:
+            optimize_expr = U.minimize_and_clip(optimizer,
+                                                weighted_error,
+                                                var_list=q_func_vars,
+                                                clip_val=grad_norm_clipping)
+        else:
+            optimize_expr = optimizer.minimize(weighted_error, var_list=q_func_vars)
 
         # update_target_fn will be called periodically to copy Q network to target Q network
         update_target_expr = []
@@ -294,11 +369,10 @@ def build_train(make_obs_ph, q_func, num_actions, optimizer, bootstrap=False, gr
                 rew_t_ph,
                 obs_tp1_input,
                 done_mask_ph,
-                importance_weights_ph,
-                update_lr_ph
+                importance_weights_ph
             ],
-            outputs=td_error[0],
-            updates=optimize_expr,
+            outputs=[td_error, errors],
+            updates=[optimize_expr]
         )
         update_target = U.function([], [], updates=[update_target_expr])
 
