e7621c5e160d6f77123b9aeccf90d07e99ddf4f8
Tim Elfrink <telfrink@mobiquityinc.com>
2018-06-26 12:29:23 +0200

    Update train.py

--- train.py
+++ train.py
@@ -255,7 +255,7 @@ if __name__ == '__main__':
                 logger.record_tabular("iters", num_iters)
                 logger.record_tabular("episodes", len(info["rewards"]))
                 logger.record_tabular("reward (100 epi mean)", np.mean(info["rewards"][-100:]))
-                logger.record_tabular("reward", info["rewards"])
+                logger.record_tabular("reward", info["rewards"][-1])
                 if not args.bootstrap:
                     logger.record_tabular("head for episode", (head+1))
                 if not args.noisy:
