8abb571c08e1649a069d06005bce820999b2aab6
Tim Elfrink <telfrink@mobiquityinc.com>
2018-06-27 10:12:13 +0200

    Update model_gym.py

--- model_gym.py
+++ model_gym.py
@@ -45,3 +45,29 @@ def bootstrap_model(img_in, num_actions, scope, reuse=False):
                 out_list.append(out_temp)
             
         return out_list
+    
+    
+def _mlp(hiddens, inpt, num_actions, scope, reuse=False, layer_norm=False):
+    with tf.variable_scope(scope, reuse=reuse):
+        out = inpt
+        for hidden in hiddens:
+            out = layers.fully_connected(out, num_outputs=hidden, activation_fn=None)
+            if layer_norm:
+                out = layers.layer_norm(out, center=True, scale=True)
+            out = tf.nn.relu(out)
+        q_out = layers.fully_connected(out, num_outputs=num_actions, activation_fn=None)
+        return q_out
+
+
+def mlp(hiddens=[], layer_norm=False):
+    """This model takes as input an observation and returns values of all actions.
+    Parameters
+    ----------
+    hiddens: [int]
+        list of sizes of hidden layers
+    Returns
+    -------
+    q_func: function
+        q_function for DQN algorithm.
+    """
+    return lambda *args, **kwargs: _mlp(hiddens, layer_norm=layer_norm, *args, **kwargs)
