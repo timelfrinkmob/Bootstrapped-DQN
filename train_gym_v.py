780c49f4ba30713746a6c64bd3e3447d505664e0
Tim Elfrink <telfrink@mobiquityinc.com>
2018-06-27 09:24:40 +0200

    Create train_gym_v.py

---
+++ train_gym_v.py
@@ -0,0 +1,25 @@
+import gym
+
+from model_gym import model, bootstrap_model
+
+
+def main():
+    env = gym.make("MountainCar-v0")
+    # Enabling layer_norm here is import for parameter space noise!
+    act = deepq.learn(
+        env,
+        q_func=model,
+        lr=1e-3,
+        max_timesteps=100000,
+        buffer_size=50000,
+        exploration_fraction=0.1,
+        exploration_final_eps=0.1,
+        print_freq=1,
+        bootstrap = False,
+        noisy = False,
+        greedy = False
+    )
+
+
+if __name__ == '__main__':
+    main()
